6a5756fdfa7daf16fe6d3b19f0c6b82fd7a8f945
rvwvmsf8wn-dot <rvwvmsf8wn@privaterelay.appleid.com>
2026-05-17 11:48:15 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = “thuiskalm-v1”;
+const BESTANDEN = [”/”, “/index.html”, “/manifest.json”];
+
+self.addEventListener(“install”, e => {
+e.waitUntil(
+caches.open(CACHE).then(cache => cache.addAll(BESTANDEN))
+);
+self.skipWaiting();
+});
+
+self.addEventListener(“activate”, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+)
+);
+self.clients.claim();
+});
+
+self.addEventListener(“fetch”, e => {
+e.respondWith(
+caches.match(e.request).then(cached => cached || fetch(e.request))
+);
+});
